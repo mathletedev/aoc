0edd2ec3c79c8fa12b5fa9c168ed24e9a06db049
Neal Wang <nealwang.sh@protonmail.com>
2024-11-11 22:59:12 -0800

    refactor: condense haskell day 09

--- 2023/haskell/src/Days/Day09.hs
+++ 2023/haskell/src/Days/Day09.hs
@@ -2,18 +2,11 @@ module Days.Day09 where
 
 import Data.List.Split
 
--- | Calculate the difference between adjacent elements in a list
-diff :: (Num a) => [a] -> [a]
-diff [] = []
-diff (x : xs)
-  | null xs = []
-  | otherwise = (head xs - x) : diff xs
-
 -- | Extrapolate the next number in a sequence
 extrapolate :: [Int] -> Int
 extrapolate xs
   | all (== 0) xs = head xs
-  | otherwise = last xs + extrapolate (diff xs)
+  | otherwise = last xs + extrapolate (zipWith (-) (tail xs) xs)
 
 part1 :: String -> String
 part1 =
